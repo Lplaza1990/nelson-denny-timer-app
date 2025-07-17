49c0fb7fa4e005cf9088e0464e29362d7c76d80c
Lplaza1990 <lucyandwalter@icloud.com>
2025-07-16 21:52:54 -0400

    initial app upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Nelson-Denny Timer App</h1>
+      <p>Interactive quiz will go here.</p>
+    </div>
+  );
+}
+
+export default App;
